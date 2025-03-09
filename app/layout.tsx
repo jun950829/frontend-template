1e1dc0fe455358cf9bd103e097f9b622113b2996
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-10 01:22:59 +0900

    frontend template - tailwind, zustand, tanstack query, dark mode sample

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/style/global.css";
+import QueryClientProvider from "@/provider/QueryClientProvider";
+import ThemeProvider from "@/provider/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +24,17 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <QueryClientProvider>
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </QueryClientProvider>  
       </body>
     </html>
   );
